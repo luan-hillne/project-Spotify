e0416e934ea9b2f8b7e19a112d86edbe39420ae6
luan-hillne <lluan8786@gmail.com>
2024-01-05 11:55:10 +0700

    update file

--- Load.py
+++ Load.py
@@ -14,11 +14,12 @@ DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
 if __name__ == "__main__":
 
 #Importing the songs_df from the Extract.py
-    load_df=Extract.search_for_track()
-    if(Transform.Data_Quality(load_df) == False):
+    album_df, artist_df, track_df = Extract.search_for_track("Tom")
+    check_song = Transform.song_df(album_df, artist_df, track_df)
+    if(Transform.Data_Quality(check_song) == False):
         raise ("Failed at Data Validation")
-    Transformed_df=Transform.Transform_df(load_df)
-    #The Two Data Frame that need to be Loaded in to the DataBase
+    transformed_album, transformed_artist, transformed_track = Transform.Transform_df(album_df, artist_df, track_df)
+    #The Three Data Frame that need to be Loaded in to the DataBase
 
 #Loading into Database
     engine = sqlalchemy.create_engine(DATABASE_LOCATION)
@@ -27,42 +28,50 @@ if __name__ == "__main__":
 
     #SQL Query to Create Track Songs
     sql_query_1 = """
-    CREATE TABLE IF NOT EXISTS my_played_tracks(
-        id_album VARCHAR(50),
-        name_track VARCHAR(200),
-        uri VARCHAR(50),
-        artist_name VARCHAR(200),
-        date VARCHAR(50),
-        top_track INT,
-        CONSTRAINT primary_key_constraint PRIMARY KEY (id_album)
+    CREATE TABLE IF NOT EXISTS my_album(
+        album_id VARCHAR(50),
+        album_name VARCHAR(50),
+        album_release_date DATE,
+        PRIMARY KEY (album_id)
     )
     """
-    # #SQL Query to Create Most Listened Artist
-    # sql_query_2 = """
-    # CREATE TABLE IF NOT EXISTS fav_artist(
-    #     timestamp VARCHAR(200),
-    #     ID VARCHAR(200),
-    #     artist_name VARCHAR(200),
-    #     count VARCHAR(200),
-    #     CONSTRAINT primary_key_constraint PRIMARY KEY (ID)
-    # )
-    # """
+    #SQL Query to Create Most Listened Artist
+    sql_query_2 = """
+    CREATE TABLE IF NOT EXISTS my_artist(
+        artist_id VARCHAR(50),
+        artist_name VARCHAR(50),
+        artist_type VARCHAR(20),
+        PRIMARY KEY (artist_id)
+    )
+    """
+    sql_query_3 = """
+    CREATE TABLE IF NOT EXISTS my_track(
+        track_id VARCHAR(50),
+        track_name VARCHAR(50),
+        track_top INT,
+        PRIMARY KEY (track_name)
+    )
+    """
     cursor.execute(sql_query_1)
-    #cursor.execute(sql_query_2)
+    cursor.execute(sql_query_2)
+    cursor.execute(sql_query_3)
     print("Opened database successfully")
 
     #We need to only Append New Data to avoid duplicates
     try:
-        load_df.to_sql("my_played_tracks", engine, index=False, if_exists='append')
+        transformed_album.to_sql("my_album", engine, index=False, if_exists='append')
     except:
         print("Data already exists in the database")
     try:
-        Transformed_df.to_sql("fav_artist", engine, index=False, if_exists='append')
+        transformed_artist.to_sql("my_artist", engine, index=False, if_exists='append')
     except:
         print("Data already exists in the database2")
-
-    #cursor.execute('DROP TABLE my_played_tracks')
-    #cursor.execute('DROP TABLE fav_artist')
+    try:
+        transformed_track.to_sql("my_track", engine, index=False, if_exists='append')
+    except:
+        print("Data already exists in the database3")
+    #cursor.execute('DROP TABLE my_album')
+    #cursor.execute('DROP TABLE my_artist')
 
     conn.close()
     print("Close database successfully")
